557fc682d7d2834ebf167ca9d535331e1d00b56e
bradcgarner <brad@bradgarner.com>
2018-01-20 12:03:43 -0500

    minor edits

--- index.js
+++
@@ -1,6 +0,0 @@
-const showDetail = index => {
-  console.log(index);
-  const rootElement = document.getElementById('aboutCategory'+index);
-  rootElement.getElementsByClassName("aboutDetail").classList.add('hidden');
-  rootElement.getElementsByClassName('aboutDetail'+index).classList.remove('hidden');
-};
